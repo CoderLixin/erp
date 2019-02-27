b9a8d4b38914afc9da892b06ab3ab7cb1fd48234
wlx <wanglixins@163.com>
2019-02-27 15:18:52 +0800

    Commit Work Dir File

--- Emp.java
+++ Emp.java
@@ -1,3 +1,6 @@
 public class Emp implements java.io.Serializable{
     private Integer empo ;
+    public void setEmp(Iteger empno){
+        this.empo=empno;
+    }
 }
--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public Hello{
     public static void main(String [] args){
         System.out.println("Hello Git");
         System.out.println("hello word!");
+        System.out.println("你好，世界！");
+        System.out.println("git...");
         }
 }
