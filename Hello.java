d6f45fe841060e5698f85d45f4f07ea39e8b8d89
wlx <wanglixins@163.com>
2019-02-28 19:14:04 +0800

    Change Hello.java Fiel in dev branch

--- Hello.java
+++ Hello.java
@@ -1,8 +1,11 @@
+import java.util.* ;
 public Hello{
     public static void main(String [] args){
         System.out.println("Hello Git");
         System.out.println("hello word!");
         System.out.println("你好，世界！");
         System.out.println("git...");
+        Systrm.out.println("滚去学习！");
+        Date date = new Date() ;
         }
 }
